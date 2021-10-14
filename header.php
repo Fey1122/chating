5073c0e7ceb38c4a7e7eba7af012bdeedfbc7e98
fey1122 <ferrylubis22@gmail.com>
2021-10-14 09:14:25 +0700

    inisialisasi pertam kali

---
+++ header.php
@@ -0,0 +1,101 @@
+<?php 
+ session_start(); 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+ <!--css external load-->
+ <link rel="stylesheet" href="offline/bootstrap.min.css">
+
+    <link rel="stylesheet" href="offline/fontawesome.min.css">
+ <link rel="stylesheet" href="offline/all.css">
+ <link type="text/css" href="offline/jquery.dataTables.min.css' rel='stylesheet'>
+ <link type="text/css" href="offline/responsive.dataTables.min.css' rel='stylesheet'>
+ <link type="text/css" href="offline/buttons.dataTables.min.css' rel='stylesheet'>
+ 
+ <link rel="stylesheet" href="offline/jquery-ui.css">
+    <link rel="stylesheet" href="offline/emojionearea.min.css">
+
+
+ <!--javascript external load-->
+    <script src="offline/jquery-3.4.1.min.js"></script>
+    <script src="offline/fontawesome.min.js"></script>
+    <script src="offline/popper.min.js"></script>
+    <script src="offline/bootstrap.min.js"></script>
+ 
+ <script src="offline/jquery-ui.js"></script>
+    <script src="offline/emojionearea.min.js"></script>
+    <script src="offline/jquery.form.js"></script>
+
+ <!-- UIkit JS
+ <script src="offline/uikit.min.js"></script>
+ <script src="offline/uikit-icons.min.js"></script> -->
+ 
+ <script src="offline/jquery.dataTables.min.js"></script>
+ <script src="offline/dataTables.responsive.min.js"></script>
+ <script src="offline/dataTables.buttons.min.js"></script>
+ <script src="offline/buttons.colVis.min.js"></script>
+ 
+ <script src="offline/highcharts.js"></script>
+ <script src="offline/highcharts-3d.js"></script>
+ 
+ <title>BIMTEK UKPBJ</title>
+ <style>
+ .form-rounded {
+  border-radius: 50rem;
+ }
+ .btn-rounded {
+  color: #fff;
+  background-color: #007bff;
+  border-radius: 50rem;
+ }
+ .btn-rounded-success {
+  color: #fff;
+  background-color: #28a745;
+  border-radius: 50rem;
+ }
+ .btn-rounded-danger {
+  color: #fff;
+  background-color: #dc3545;
+  border-radius: 35rem;
+ }
+ .btn-rounded-info {
+  color: #fff;
+  background-color: #17a2b8;
+  border-radius: 50rem;
+ }
+ .btn-rounded-light {
+  color: #343a40;
+  background-color: #f8f9fa;
+  border-radius: 50rem;
+ }
+ .btn-rounded-dark {
+  color: #fff;
+  background-color: #343a40;
+  border-radius: 50rem;
+ }
+ .btn-rounded-warning {
+  color: #fff;
+  background-color: #ffc107;
+  border-radius: 50rem;
+ }
+ .btn-rounded-secondary {
+  color: #fff;
+  background-color: #868e96;
+  border-radius: 50rem;
+ }
+ /*
+ .btn-success {
+    color: #fff;
+    background-color: #28a745;
+    border-radius: 35px;
+    border: 1px solid rgba(21, 87, 36, 0.75);
+ }
+ */ </style>
+</head>
+<body>
+    <div class="container">
